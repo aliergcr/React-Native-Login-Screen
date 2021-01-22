e6fd84d56dbf28f68ba107278b44371cd7bb3508
Ali Ergeçer <46872079+aliergcr@users.noreply.github.com>
2021-01-22 23:29:22 +0300

    update

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './authReducer';
-import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
   auth: authReducer,
-  user: userReducer,
 });
