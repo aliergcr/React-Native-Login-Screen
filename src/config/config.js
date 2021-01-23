e4f99c4ab5d68b6e6503749df4c6007fdf5b655f
Ali Ergeçer <46872079+aliergcr@users.noreply.github.com>
2021-01-23 16:44:12 +0300

    update

--- src/config/config.js
+++ src/config/config.js
@@ -2,13 +2,13 @@ import firebase from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 
 let config = {
-  apiKey: 'AIzaSyA84kGIeM9kq5GS1oNoUyhCfas51ERM4gA',
-  authDomain: 'loginapp-ab6a6.firebaseapp.com',
-  projectId: 'loginapp-ab6a6',
-  storageBucket: 'loginapp-ab6a6.appspot.com',
-  messagingSenderId: '439000684413',
-  appId: '1:439000684413:web:d1d00bcd341df6af169e14',
-  measurementId: 'G-4RQQ2E23WG',
+  apiKey: '**************',
+  authDomain: '**************',
+  projectId: '**************',
+  storageBucket: '**************',
+  messagingSenderId: '**************',
+  appId: '**************',
+  measurementId: '**************',
 };
 
 //firebase.initializeApp(config);
